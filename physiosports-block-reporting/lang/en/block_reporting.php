c36b3798f4ed95e692b9cb1af0067e919fd2c4a0
Faneeza-Ashraf <faneezaashraf257@gmail.com>
2025-11-29 07:57:58 +0500

    Add files via upload

---
+++ physiosports-block-reporting/lang/en/block_reporting.php
@@ -0,0 +1,105 @@
+<?php
+    $string['pluginname'] = 'Reporting Block';
+    $string['reporting'] = 'Reporting';
+    $string['reporting:addinstance']  = 'Add a new reporting block';
+    $string['reportsetting']  = 'Report Setting';
+    $string['pie_completed']  = '<h3> Pie chart </h3> Course completed color: ';
+    $string['pie_not_completed']  = ' Course not completed color: ';
+    $string['pie_completed_highlight']  = ' Course completed highlight color: ';
+    $string['pie_not_completed_highlight']  = ' Course not completed highlight color: ';
+    $string['bar_completed']  = '<h3> Bar chart </h3> Course completed color: ';
+    $string['bar_net_completed']  = ' Course not completed color: ';
+    $string['courseoverview_percentage']  = '<h3>Course overview</h3>  Percentage color: ';
+    $string['courseoverview_bg']  = ' Background color: ';
+    $string['installhierarchy'] = 'Please setup your hierarchy tree before viewing hierarchy reports.!';
+    $string['couldnotfinduser'] = "<div class='alert alert-error'>No user was found that matched your input: <strong>{name}</strong></div>";
+    $string['savemessage'] = '<div class="alert alert-success">Changes saved</div>';
+    $string['reportsettingheader'] = "<h2 class='font-weight-bold section-title mb-3 color-light-blue'>Report Setting</h2>";
+    $string['filter_reports'] = 'Filter reports';
+    $string['filter_name'] = 'Filter name';
+    $string['filter_applied'] = 'Filter applied';
+    $string['noresult'] = 'No results were found for your selection.';
+    $string['applied_filters'] = 'Applied filters';
+
+    $string['general_reports'] = 'General reports';
+    $string['individual_reports'] = 'Individual reports';
+    $string['activity_reports'] = 'Activity reports';
+    $string['course_overview_reports'] = 'Course overview reports';
+    $string['users_reports'] = 'User reports';
+    $string['reporting_filter'] = 'Reporting filter';
+    $string['reporting_setting'] = 'Report setting';
+
+    $string['course_name'] = 'Course name';
+    $string['course'] = 'Course';
+    $string['category'] = 'Category';
+    $string['user'] = 'User';
+    $string['module'] = 'Module';
+    $string['completion'] = 'Completion';
+    $string['export_exel_csv'] = 'Export Excel/CSV';
+    $string['fill_out_fields_you_wish'] = 'Only fill out fields you wish to filter with.';
+    $string['enrolled_date'] = 'Enrolled date';
+    $string['before'] = 'Before';
+    $string['after'] = 'After';
+    $string['completion_date'] = 'Completion date';
+    $string['hierarchy'] = 'Hierarchy';
+    $string['search_for_node'] = 'Search for a node';
+    $string['current_selected_hierarchy'] = 'Current hierarchy selection';
+    $string['suspended_users'] = 'Suspended users';
+    $string['exclude_suspended_users'] = 'Exclude suspended users';
+    $string['include_suspended_users'] = 'Include suspended users';
+    $string['show_suspended_users_only'] = 'Show suspended users only';
+    $string['display_type'] = 'Display type';
+    $string['select_to_print'] = 'Select this option to print';
+    $string['go'] = 'Go';
+    $string['status'] = 'Status';
+
+    $string['sorting_tip'] = 'Tip: Sort multiple columns simultaneously by holding down the shift key and clicking a second, third or even fourth column header!';
+    $string['course_progress'] = 'Course progress';
+    $string['overall_progress'] = 'Overall progress';
+    $string['selected_hierarchy_progress'] = 'Selected hierarchy progress';
+    $string['completed'] = 'Completed';
+    $string['not_completed'] = 'Not completed';
+
+    $string['generalresult'] = '<h2> General Report Results </h2>';
+    $string['individualresult'] = '<h2> Individual Report Results</h2>';
+    $string['title:individual'] = '<h2> Individual Report</h2>';
+    $string['activityresult'] = '<h2> Activity Report Results</h2>';
+    $string['courseoverviewresult'] = '<h2> Course Overview Report Results</h2>';
+
+    $string['vendorsetting']  = 'Vendor Setting';
+    $string['vendorsheader'] = "<h2>Vendor</h2>";
+    $string['vendorsuserheader'] = "<h2>Vendor Users</h2>";
+    $string['vendorsettingheader'] = "<h2>Vendor Setting</h2>";
+    $string['notallowtoaccess'] = '<div class="alert alert-error">You are not allowed to access this page</div>';  
+    $string['vendorname'] = "<div class='label_text'>Vendor name: </div>";
+    $string['username'] = "<div class='label_text'>User: </div>";
+    $string['couldnotfinduseras'] = '<div class="alert alert-error">Could not find any user as </div>';
+    $string['error:selectusername'] = '<div class="alert alert-error">Please select a name!</div>';
+    $string['error:couldnotfinduser'] = '<div class="alert alert-error">Could not find any user!</div>';
+
+    $string['generalfields']='<b>Default fields</b>';
+    $string['userdefinedfields']='<b>User defined fields</b>';
+
+    $string['reporting:changereportsfilter'] = 'Change reports filters';
+    $string['reporting:viewreports'] = 'View all reports';
+
+//OTHER SETTING
+$string['othersetting']  = '<h3>PDF settings</h3>';
+$string['pageorientation']  = 'Page orientation';
+$string['reportlogo']  = 'Report header logo';
+$string['reportlogo:hint']  = 'Report logo will be advised good at ...';
+
+//Added by Muskan Arshad//
+$string['reporting:myaddinstance'] ='Reporting Block';
+$string['reporting:myaddinstance'] ='Add a new Coupon block to My Page';
+
+$string['pluginname'] = 'Reporting Block';
+$string['reporting:addinstance'] = 'Add a new Reporting block';
+$string['reporting:myaddinstance'] = 'Add a new Reporting block to My Page';
+$string['reporting:viewaddupdatemodule'] = 'View and update module in Reporting block';
+
+$string['reporting:viewdeletemodule'] = 'View and delete module in Reporting block';
+/*Added by Muskan*/
+$string['display_icq'] = 'Display ICQ';
+
+$string['display_skype'] = 'Display Skype';
